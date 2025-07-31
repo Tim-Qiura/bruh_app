1f843669cf91dfd5f3bcf2a5e267538b06ac2f64
Tim-Qiura <kiuratim18@gmail.com>
2025-07-31 17:26:27 +0300

    second commit

--- app/src/main/java/com/example/bruh/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/bruh/navigation/AppNavHost.kt
@@ -7,13 +7,15 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.bruh.ui.theme.screens.dashboard.dashboardScreen
 import com.example.bruh.ui.theme.screens.login.loginScreen
+import com.example.bruh.ui.theme.screens.patients.AddPatientScreen
 import com.example.bruh.ui.theme.screens.register.registerScreen
 
 @Composable
-fun AppNavHost(navController: NavHostController= rememberNavController(),startDestination:String= ROUTE_REGISTER){
+fun AppNavHost(navController: NavHostController= rememberNavController(),startDestination:String= ROUTE_DASHBOARD){
     NavHost(navController=navController,startDestination=startDestination){
         composable(ROUTE_REGISTER) { registerScreen(navController) }
         composable(ROUTE_LOGIN) { loginScreen(navController) }
         composable(ROUTE_DASHBOARD) { dashboardScreen(navController) }
+        composable(ROUTE_ADDPATIENT) { AddPatientScreen(navController) }
     }
 }
